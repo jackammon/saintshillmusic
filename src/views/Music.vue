d61f74fc649c58b42f9be156173f0d932c0d5327
Jack Ammon <hello@jck.codes>
2024-08-05 14:14:24 -0700

    fix things

--- src/views/Music.vue
+++ src/views/Music.vue
@@ -7,7 +7,7 @@
 <script>
 import AlbumRow from '../components/AlbumRow.vue';
 import dayjs from 'dayjs';
-import imageUrl from '@/assets/images/family.webp'
+import imageUrl from '@/assets/family.webp'
 
 export default {
   components: {
