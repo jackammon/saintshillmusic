39d2757a9c7eae6b21cfa51111f1a5a6c90253af
Jack Ammon <hello@jck.codes>
2024-08-08 18:42:57 -0700

    added lp images

--- src/views/Merch.vue
+++ src/views/Merch.vue
@@ -1,81 +1,80 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col>
-        <div class="effect">
-          Pre-Order "Family" on Vinyl August 18th
-        </div>
+  <v-container class="content-offset">
+    <v-row>
+      <v-col cols="12">
+        <img src="../assets/lp_b.webp" alt="Merch 1" class="merch-image" id="merch-1"/>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <img src="../assets/lp_c.webp" alt="Merch 2" class="merch-image overlap-image" id="merch-2"/>
+      </v-col>
+      <v-col cols="12">
+        <!-- <img src="../assets/merch3.webp" alt="Merch 3" class="merch-image"/> -->
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <style scoped>
-
 a {
   color: black;
   text-decoration: none;
 }
 
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background: #262626;
+.content-offset {
+  margin-top: -200px;
 }
 
-h1 {
-    font-size: 5vw !important;
-    line-height: 90%;
-    margin-bottom: 1vw;
+.merch {
+  max-width: 100%;
+  height: auto;
+}
+
+.merch-image {
+  max-width: 95vw;
+  height: auto;
+  margin-bottom: 1rem;
+}
+
+.overlap-image {
+  margin-top: -450px; 
+  z-index: 10;
+   
+}
+
+@media (max-width: 1300px) {
+ .overlap-image {
+    margin-top: -350px; 
+    z-index: 10; 
   }
-
-h2 {
-  font-style: normal;
-  font-size: 1.5vw;
-  line-height: 110%;
-  font-weight: 500;
-  margin-bottom: 2vw;
-}
-.contact {
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  font-family: 'Rubik' !important;
-  text-transform: uppercase;
-  color: black;
-  letter-spacing: -.02em !important;
-  text-align: left;
-}
-
-/* Responsive adjustments for tablets */
-@media (max-width: 960px) {
-  h1 {
-    font-size: 7vw !important;
-  }
-
-  h2 {
-    font-size: 2.5vw !important;
-    margin-bottom: 4vw !important;
+  .content-offset {
+    margin-top: -100px;
   }
 }
 
-/* Responsive adjustments for mobile phones */
-@media (max-width: 480px) {
-  .contact {
-    top: 47%; /* adjust position slightly for better viewing */
-    left: 50%;
-    transform: translate(-50%, -40%);
+@media (max-width: 1100px) {
+ .overlap-image {
+    margin-top: -350px; 
+    z-index: 10; 
+  }
+  .content-offset {
+    margin-top: -100px;
   }
 }
-
-@media (max-width: 315px) {
-  .contact {
-    top: 45%; /* adjust position slightly for better viewing */
-    left: 50%;
-    transform: translate(-50%, -40%);
+ @media (max-width: 900px) {
+ .overlap-image {
+    margin-top: -300px; 
+    z-index: 10; 
   }
 }
-
+ @media (max-width: 700px) {
+ .overlap-image {
+    margin-top: -150px; 
+    z-index: 10; 
+  }
+    .content-offset {
+    margin-top: -50px;
+  }
+}
 </style>
