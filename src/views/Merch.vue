d20c3f841686cf1826d13e9bb5c46b32c5377568
Jack Ammon <hello@jck.codes>
2024-08-09 12:30:54 -0700

    try again

--- src/views/Merch.vue
+++ src/views/Merch.vue
@@ -10,7 +10,7 @@
         <img src="../assets/lp_c.webp" alt="Merch 2" class="merch-image offset-2" id="merch-2"/>
       </v-col>
       <v-col cols="12">
-          <img src="../assets/lp_a.webp" alt="Merch 3" class="merch-image offset-3" />
+          <img src="../assets/merch.webp" alt="Merch 3" class="merch-image offset-3" />
       </v-col>
     </v-row>
   </v-container>
