d85d6b6ee0f92612ce479c40ba7ca3e6bb0729f2
Jack Ammon <hello@jck.codes>
2025-11-22 17:36:54 -0800

    Update single release text in Linktree.vue for clarity

--- src/views/Linktree.vue
+++ src/views/Linktree.vue
@@ -36,7 +36,7 @@
       <a href="https://open.spotify.com/artist/7zVhbvqRXFzIdF28uXV09A" target="_blank">
         <img src="@/assets/unto_us.jpeg" alt="FOREVER AND EVER (Emmanuel) Single" />
       </a>
-      <p class="single-text">New Single - Out Friday, Nov 28th</p>
+      <p class="single-text">FOREVER AND EVER (Emmanuel) - Out Friday, Nov 28th</p>
     </div>
 
     <!-- YouTube Video Card -->
